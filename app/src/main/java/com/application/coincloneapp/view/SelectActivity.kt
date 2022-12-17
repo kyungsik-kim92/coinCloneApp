291ce30e332fd00d0be2b36a674ddc3a29e3ad24
KIMMKyungsik <ksiki1@naver.com>
2022-12-18 00:49:02 +0900

    리사이클러뷰 어댑터 생성 후 연결

--- app/src/main/java/com/application/coincloneapp/view/SelectActivity.kt
+++ app/src/main/java/com/application/coincloneapp/view/SelectActivity.kt
@@ -4,20 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.application.coincloneapp.R
+import com.application.coincloneapp.databinding.ActivitySelectBinding
+import com.application.coincloneapp.view.adapter.SelectRVAdapter
 import timber.log.Timber
 
 class SelectActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivitySelectBinding
+
     private val viewModel: SelectViewModel by viewModels()
+
+    private lateinit var selectRVAdapter: SelectRVAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select)
+        binding = ActivitySelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.getCurrentCoinList()
         viewModel.currentPriceResult.observe(this, Observer {
+
+            selectRVAdapter = SelectRVAdapter(this, it)
+
+            binding.coinListRV.adapter =selectRVAdapter
+            binding.coinListRV.layoutManager = LinearLayoutManager(this)
+
             Timber.d(it.toString())
         })
 
