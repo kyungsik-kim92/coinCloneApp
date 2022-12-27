13e2e7c4a142d9f8e2e4c37c0d4e6932f04c808e
KIMMKyungsik <ksiki1@naver.com>
2022-12-28 01:55:45 +0900

    리사이클러뷰에 코인 리스트 연결

--- app/src/main/java/com/application/coincloneapp/view/adapter/CoinListRVAdapter.kt
+++ app/src/main/java/com/application/coincloneapp/view/adapter/CoinListRVAdapter.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.application.coincloneapp.R
 import com.application.coincloneapp.db.entity.InterestedCoinEntity
@@ -12,11 +15,17 @@ class CoinListRVAdapter(val context: Context, val dataSet: List<InterestedCoinEn
     RecyclerView.Adapter<CoinListRVAdapter.ViewHolder>() {
 
 
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val coinName = view.findViewById<TextView>(R.id.coinName)
+        val likeBtn = view.findViewById<ImageView>(R.id.likeBtn)
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.main_coin_item,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.main_coin_item, parent, false)
 
         return ViewHolder(view)
 
@@ -24,12 +33,26 @@ class CoinListRVAdapter(val context: Context, val dataSet: List<InterestedCoinEn
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
+        holder.coinName.text = dataSet[position].coin_name
+
+        val selected = dataSet[position].selected
+
+        if (selected) {
+            holder.likeBtn.setImageResource(R.drawable.like_red)
+
+
+        }else{
+
+            holder.likeBtn.setImageResource(R.drawable.like_grey)
+        }
+
+
     }
 
     override fun getItemCount(): Int {
 
         return dataSet.size
 
-        }
-
     }
+
+}
