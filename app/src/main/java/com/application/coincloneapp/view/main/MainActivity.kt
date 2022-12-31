b79169017bab2182aab6b604cd53795f87de9286
KIMMKyungsik <ksiki1@naver.com>
2022-12-31 09:44:30 +0900

    포그라운드 서비스 이용해보기

--- app/src/main/java/com/application/coincloneapp/view/main/MainActivity.kt
+++ app/src/main/java/com/application/coincloneapp/view/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.application.coincloneapp.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
@@ -7,6 +8,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.application.coincloneapp.R
 import com.application.coincloneapp.databinding.ActivityMainBinding
 import com.application.coincloneapp.databinding.ActivitySelectBinding
+import com.application.coincloneapp.view.setting.SettingActivity
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.setting.setOnClickListener {
+            val intent = Intent(this,SettingActivity::class.java)
+            startActivity(intent)
+        }
 
         val bottomNavigationView = binding.bottomNavigationView
         val navController = findNavController(R.id.fragmentContainerView)
---
+++ app/src/main/java/com/application/coincloneapp/view/setting/SettingActivity.kt
@@ -0,0 +1,41 @@
+package com.application.coincloneapp.view.setting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.application.coincloneapp.R
+import com.application.coincloneapp.databinding.ActivityMainBinding
+import com.application.coincloneapp.databinding.ActivitySettingBinding
+import com.application.coincloneapp.service.PriceForegroundService
+
+class SettingActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySettingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.startForeground.setOnClickListener {
+
+            Toast.makeText(this,"start",Toast.LENGTH_LONG).show()
+            val intent = Intent(this, PriceForegroundService::class.java)
+            intent.action = "START"
+            startService(intent)
+
+        }
+
+        binding.stopForeground.setOnClickListener {
+            Toast.makeText(this,"stop",Toast.LENGTH_LONG).show()
+            val intent = Intent(this, PriceForegroundService::class.java)
+            intent.action = "STOP"
+            startService((intent))
+
+        }
+
+    }
+}
